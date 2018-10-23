0e0f9e33842eae190f06b09f4e55e948379c8f5d
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-10-22 23:09:49 -0700

    factor out blog body

---
+++ src/components/post-body.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
+
+const PostBody = ({ children, post, title }) => (
+  <article className="card blogPost">
+    <div className="card-content">
+      <div className="media blogHeader">
+        <div className="media-content has-text-centered">
+          <div
+            className="image authorImage"
+            style={{
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              position: 'relative',
+              backgroundImage: `url('${post.avatar}')`
+            }}
+          >
+            <svg
+              width="80"
+              height="80"
+              style={{
+                width: '100%',
+                height: 'auto',
+                maxWidth: '100%',
+                marginBottom: -4
+              }}
+            />
+          </div>
+          <h1 className="title blogTitle">{title || post.title}</h1>
+          <p className="subtitle is-6 blogSubtitle">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              to={post.authorLink}
+              href={post.authorLink}
+            >
+              {post.author}
+            </a>
+            <span> on {dayjs(post.publishDate).format('MMMM D, YYYY')}</span>
+          </p>
+        </div>
+      </div>
+      <div className="blogBody">{children}</div>
+    </div>
+
+    <style jsx>
+      {`
+        .authorImage {
+          border: 3px solid #ccc;
+          border-radius: 50%;
+          height: 60px;
+          left: 50%;
+          margin-left: -30px;
+          position: absolute !important;
+          top: -30px;
+          width: 60px;
+        }
+        .blogPost {
+          margin: auto auto 65px;
+          max-width: 800px;
+          width: 100%;
+          margin-top: 100px;
+        }
+        .blogTitle {
+          margin-top: 0 !important;
+          font-size: 2rem;
+          font-weight: lighter;
+          line-height: 2;
+        }
+        .blogSubtitle {
+          color: #909aa0;
+          margin-bottom: 2rem;
+        }
+        .blogHeader {
+          margin-top: 2.5rem;
+        }
+        .blogBody {
+          line-height: 1.4;
+          margin: 0 1rem;
+        }
+        @media screen and (min-width: 769px) {
+          .blogBody {
+            margin: 0 4rem;
+          }
+        }
+      `}
+    </style>
+  </article>
+);
+
+PostBody.propTypes = {
+  children: PropTypes.node.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+export default PostBody;
--- src/components/stub.js
+++ src/components/stub.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
-import dayjs from 'dayjs';
+import PostBody from './post-body';
 
 class BlogStub extends Component {
   static propTypes = {
@@ -33,62 +33,22 @@ class BlogStub extends Component {
     const { BlogPost } = this.state;
 
     return (
-      <article className="card blogPost">
-        <div className="card-content">
-          <div className="media blogHeader">
-            <div className="media-content has-text-centered">
-              <div
-                className="image authorImage"
-                style={{
-                  backgroundSize: 'cover',
-                  backgroundRepeat: 'no-repeat',
-                  position: 'relative',
-                  backgroundImage: `url('${post.avatar}')`
-                }}
-              >
-                <svg
-                  width="80"
-                  height="80"
-                  style={{
-                    width: '100%',
-                    height: 'auto',
-                    maxWidth: '100%',
-                    marginBottom: -4
-                  }}
-                />
-              </div>
-              <h1 className="title blogTitle">
-                <Link href={post.urlPath} prefetch={prefetch}>
-                  <a>{post.title}</a>
-                </Link>
-              </h1>
-              <p className="subtitle is-6 blogSubtitle">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  to={post.authorLink}
-                  href={post.authorLink}
-                >
-                  {post.author}
-                </a>
-                <span>
-                  {' '}
-                  on {dayjs(post.publishDate).format('MMMM D, YYYY')}
-                </span>
-              </p>
-            </div>
-          </div>
-          <div className="blogBody">
-            {BlogPost && <BlogPost />}
+      <PostBody
+        post={post}
+        title={
+          <Link href={post.urlPath} prefetch={prefetch}>
+            <a>{post.title}</a>
+          </Link>
+        }
+      >
+        <div className="preview">
+          {BlogPost && <BlogPost />}
+          <div className="bottomFade" />
 
-            <div className="bottomFade" />
-
-            <Link href={post.urlPath} prefetch={prefetch}>
-              <a className="readMore">Read More</a>
-            </Link>
-          </div>
+          <Link href={post.urlPath} prefetch={prefetch}>
+            <a className="readMore">Read More</a>
+          </Link>
         </div>
-
         <style jsx>
           {`
             .bottomFade {
@@ -110,49 +70,13 @@ class BlogStub extends Component {
               width: 100%;
               text-align: center;
             }
-            .authorImage {
-              border: 3px solid #ccc;
-              border-radius: 50%;
-              height: 60px;
-              left: 50%;
-              margin-left: -30px;
-              position: absolute !important;
-              top: -30px;
-              width: 60px;
-            }
-            .blogPost {
-              margin: auto auto 65px;
-              max-width: 800px;
-              width: 100%;
-              margin-top: 100px;
-            }
-            .blogTitle {
-              margin-top: 0 !important;
-              font-size: 2rem;
-              font-weight: lighter;
-              line-height: 2;
-            }
-            .blogSubtitle {
-              color: #909aa0;
-              margin-bottom: 2rem;
-            }
-            .blogHeader {
-              margin-top: 2.5rem;
-            }
-            .blogBody {
-              line-height: 1.4;
-              margin: 0 1rem;
+            .preview {
               max-height: 525px;
               overflow: hidden;
             }
-            @media screen and (min-width: 769px) {
-              .blogBody {
-                margin: 0 4rem;
-              }
-            }
           `}
         </style>
-      </article>
+      </PostBody>
     );
   }
 }
